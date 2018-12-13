b9bd88d93cacd8242f5f6390a4fbaa4eee1203ff
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-12-13 17:19:46 +0300

    Автоудаление уже распознанных файлов (ура)

--- googlecloudvision.py
+++ googlecloudvision.py
@@ -18,30 +18,46 @@ def localize_objects(path):
 
         print('Number of objects found: {}'.format(len(objects)))
         for object_ in objects:
-            print('\n{} (confidence: {})'.format(object_.name, object_.score))
+            print('{} (confidence: {})'.format(object_.name, object_.score))
                     
             box = [(vertex.x*im.width, vertex.y*im.height) for vertex in object_.bounding_poly.normalized_vertices]
                 
             if box not in allcoords:
                 allcoords.append(box)
-                print(box)
                 r = lambda: random.randint(0,255)
                 draw.line(box + [box[0]], width=5, fill='#%02X%02X%02X' % (r(),r(),r()))
                 draw.text(box[0], object_.name+" "+str(object_.score), font=ImageFont.truetype("~/Library/Fonts/MuseoSansCyrl-300.ttf",30))
         image_file.close()
         im.save("output"+path+".png")
     except:
-        print("РИП БРАТ")
-        time.sleep(30)
+        pass
             
 
 if __name__ == '__main__':
+    thisfiles = []
+    for root, dirs, files in os.walk("."):  
+        for filename in files:
+            if filename[:11] == "outputframe":
+                thisfiles.append(filename)
+    
+    for item in thisfiles:
+        formatedtext = "".join(item.split())
+        while formatedtext[-1:].isnumeric() == False:
+            formatedtext = formatedtext[:-1]
+        formatedtext = formatedtext[::-1]
+        while formatedtext[-1:].isnumeric() == False:
+            formatedtext = formatedtext[:-1]
+        try:
+            os.remove("frame"+formatedtext[::-1]+".jpg")
+        except OSError:
+            pass
+
     allfileslist = []
     for root, dirs, files in os.walk("."):  
         for filename in files:
-            if filename[-3:] == "jpg":
+            if filename[:5] == "frame":
                 allfileslist.append(filename)
     allfileslist.sort()
-    print(allfileslist)
     for this_image in allfileslist:
+        print("\n***Working with "+this_image+"***")
         localize_objects(this_image)
